2bfbacc917555a75d03e5e8a07527aad0609306a
vyunak <lubim4ikvlad@gmail.com>
2020-07-23 21:30:44 +0300

    web api

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-var noble = require('noble');
-var mathjs = require('mathjs');
+let noble = require('noble');
+let mathjs = require('mathjs');
+let express = require('express');
+const app = express();
 
-var state = {
-	color: null,
+let state = {
+	color: {
+		r: 255,
+		g: 255,
+		b: 255
+	},
 	code: null,
 	id: 'ff:ff:38:59:a8:31'
 }
@@ -26,7 +32,7 @@ noble.on('warning', (message) => {
 
 noble.on('discover', (peripheral) => {
 
-	if (peripheral.address == state.id || peripheral.id == state.id) {
+	if (peripheral.address === state.id || peripheral.id === state.id) {
 
 		peripheral.on('disconnect', function() {
 			process.exit(0);
@@ -34,29 +40,132 @@ noble.on('discover', (peripheral) => {
 
 		peripheral.connect((error) => {
 
-			var serviceUUIDs = ["ffd5", "ffd0"];
-			var characteristicUUIDs = ["ffd9", "ffd4"];
+			const serviceUUIDs = ["ffd5", "ffd0"];
+			const characteristicUUIDs = ["ffd9", "ffd4"];
 			peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, (error, services, characteristics) => {
 
-				characteristic = characteristics[0];
+				console.log('connected');
+				let characteristic = characteristics[0];
 
-				characteristics[0].read((error, data) => {
+				app.get('/', function (req, res) {
+					res.send('Hello World')
+				})
 
-					if (HTS(data[0]) == '56' || HTS(data[0]) == '0')
+				app.get('/api/switchColor/:r/:g/:b/:time', function (req, res) {
+					console.log(`/api/switchColor/${req.params.r}/${req.params.g}/${req.params.b}`, new Date());
+					let time = parseInt(req.params.time);
+					(isNaN(time) || time < 100) ? time = 100 : null;
+					if (req.params.r != null && req.params.g != null && req.params.b != null)
 					{
-						state.color = {
-							r: data[1] == 0 ? 255 : data[1],
-							g: data[2] == 0 ? 255 : data[2],
-							b: data[3] == 0 ? 255 : data[3]
+						let RColor = parseInt(req.params.r) % 256;
+						let GColor = parseInt(req.params.g) % 256;
+						let BColor = parseInt(req.params.b) % 256;
+						let colors = {
+							r: RColor,
+							b: BColor,
+							g: GColor
+						};
+						if (!isNaN(RColor) && !isNaN(GColor) && !isNaN(BColor)) {
+							switchColor(characteristic, colors, time, 0xFF);
+							res.send(colors)
+						} else {
+							res.send({error: 'color error'})
 						}
-						switchColor(characteristics[0], state.color, 250, 0xF0);
-						console.log(state.color);
+					} else {
+						res.send({error: 'not all params'})
 					}
 				})
+
+				app.get('/api/toggle', (req, res) => {
+					console.log(`/api/toggle`, new Date());
+					let state = {
+						power: false,
+						all: null
+					}
+					characteristic.read((err, date) => {
+						state.all = date.toString('hex');
+						let cmd = HTS(date[0]);
+						if (cmd === '56') {
+							state.power = true;
+						} else if (cmd === 'cc') {
+							state.power = HTS(date[1]) === '23';
+						} else if (cmd === '0') {
+							state.power = true;
+						}
+						let buf = new Buffer([0xCC, state.power === false ? 0x23 : 0x24, 0x33]);
+						characteristic.write(buf);
+						return res.send(state);
+					})
+				})
+
+				app.get('/api/toggle/:state', function (req, res) {
+					console.log(`/api/toggle/${req.params.state}`, new Date());
+					if (req.params.state != null)
+					{
+						let state = parseInt(req.params.state) % 2;
+						if (!isNaN(state))
+						{
+							let buf = new Buffer([0xCC, state === 1 ? 0x23 : 0x24, 0x33]);
+							characteristic.write(buf);
+							res.send({})
+						} else {
+							res.send({error: 'state error'})
+						}
+					} else {
+						res.send({error: 'not all params'})
+					}
+				})
+
+				app.get('/api/getState', (req, res) => {
+					console.log('/api/getState', new Date());
+					let state = {
+						power: false,
+						all: null
+					}
+					characteristic.read((err, date) => {
+						state.all = date.toString('hex');
+						let cmd = HTS(date[0]);
+						if (cmd === '56') {
+							state.power = true;
+						} else if (cmd === 'cc') {
+							state.power = HTS(date[1]) === '23';
+						} else if (cmd === '0') {
+							state.power = true;
+						}
+						return res.send(state);
+					})
+				})
+
+				// characteristics[0].read((error, data) => {
+
+				// 	if (HTS(data[0]) == '56' || HTS(data[0]) == '0')
+				// 	{
+				// 		state.color = {
+				// 			r: data[1] == 0 ? 255 : data[1],
+				// 			g: data[2] == 0 ? 255 : data[2],
+				// 			b: data[3] == 0 ? 255 : data[3]
+				// 		}
+				// 		switchColor(characteristics[0], state.color, 250, 0xF0);
+				// 		console.log(state.color);
+				// 	}
+
+				// 	console.log('=================================');
+				// })
 
 				// characteristic.on('write', () => {
 					
 				// })
+
+				// setInterval(() => {
+				//
+				// 	let color = {
+				// 		r: mathjs.randomInt(0, 255),
+				// 		g: mathjs.randomInt(0, 255),
+				// 		b: mathjs.randomInt(0, 255)
+				// 	};
+				//
+				// 	switchColor(characteristic, color, 1500, 0xFF);
+				// }, 2000)
 
 				function HTS(num) {
 					return Number(num).toString(16);
@@ -70,52 +179,46 @@ noble.on('discover', (peripheral) => {
 
 					(time < 100) ? time = 100 : null;
 
-					prevColor = state.color;
-					console.log(prevColor, color);
+					let prevColor = {
+						r: state.color.r,
+						g: state.color.g,
+						b: state.color.b,
+					};
+					state.color = color;
+					// console.log(prevColor, 'prev', color, 'next');
 
-					animationSpeed = 10;
-					timeInterval = time / animationSpeed;
+					let animationSpeed = 50;
+					let timeInterval = time / animationSpeed;
 
-					perColorR = calcMinus(prevColor.r, color.r) / timeInterval;
-					perColorG = calcMinus(prevColor.g, color.g) / timeInterval;
-					perColorB = calcMinus(prevColor.b, color.b) / timeInterval;
+					let perColorR = calcMinus(prevColor.r, color.r) / timeInterval;
+					let perColorG = calcMinus(prevColor.g, color.g) / timeInterval;
+					let perColorB = calcMinus(prevColor.b, color.b) / timeInterval;
 
-					v = 0;
-					interval = setInterval(() => {
+					let v = 0;
+					let interval = setInterval(() => {
 						v += animationSpeed;
 
-						prevColor.r = prevColor.r - perColorR;
-						prevColor.g = prevColor.g - perColorG;
-						prevColor.b = prevColor.b - perColorB;
+						prevColor.r -= perColorR;
+						prevColor.g -= perColorG;
+						prevColor.b -= perColorB;
 
 
-						var red = prevColor.r;
-						var green = prevColor.g;
-						var blue = prevColor.b;
+						let red = prevColor.r;
+						let green = prevColor.g;
+						let blue = prevColor.b;
 
-						var buf = new Buffer([0x56, red, green, blue, brightness, 0xF0 ,0xAA]);
+						let buf = new Buffer([0x56, red, green, blue, 0x00, 0xF0 ,0xAA]);
 
 						characteristic.write(buf);
 
 						if (v >= time)
 							clearInterval(interval);
-
+						
 					}, animationSpeed)
-
-					state.color = color;
 
 				}
 
-				setInterval(() => {
 
-					color = {
-						r: mathjs.randomInt(0, 179),
-						g: mathjs.randomInt(0, 179),
-						b: mathjs.randomInt(0, 179)
-					};
-
-					switchColor(characteristics[0], color, 250, 0xF0);
-				}, 333)
 
 			});
 
@@ -123,3 +226,5 @@ noble.on('discover', (peripheral) => {
 
 	}
 });
+
+app.listen(3000)
